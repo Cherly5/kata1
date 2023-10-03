10c9a3deb054df258442a0f7d6ff9fd4d8bcc61e
Echeyde Ramos Caballero <echeyde.ramos@gmail.com>
2023-10-03 17:17:23 +0100

    commit 2

--- src/kata1/Main.java
+++ src/kata1/Main.java
@@ -1,4 +1,12 @@
 package kata1;
 
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2001,04,25);
+
+        Person person = new Person("Echeyde",date);
+        System.out.println(person.getName() + " tiene " + person.getAge() + " años.");
+    }
 }
